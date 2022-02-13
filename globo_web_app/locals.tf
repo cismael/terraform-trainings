0ec83b4999d83360cf9e0360de9082b7222e6063
cismael <c.ismael@live.fr>
2022-02-13 23:09:41 +0100

    [globo_web_app] : Add s3 bucket

--- globo_web_app/locals.tf
+++ globo_web_app/locals.tf
@@ -1,13 +1,16 @@
-locals {
-    common_tags = {
-        company = var.company
-        project = "${var.company}-${var.project}"
-        billing_code = var.billing_code
-    }
-}
-
 # The following will generate a random "rand"integer between 10000 and 99999:
 resource "random_integer" "rand" {
   min = 10000
   max = 99999
 }
+
+locals {
+  common_tags = {
+    company      = var.company
+    project      = "${var.company}-${var.project}"
+    billing_code = var.billing_code
+  }
+
+  s3_bucket_name = "globo-web-app-${random_integer.rand.result}"
+}
+
